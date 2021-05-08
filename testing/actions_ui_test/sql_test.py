6290aa81ebf71595bf20c72d6c5afc71ebe4153e
timo364 <timoxa.or12@gmail.com>
2021-05-08 22:17:45 +0300

    last commit today.

---
+++ testing/actions_ui_test/sql_test.py
@@ -0,0 +1,66 @@
+import sys
+from PyQt5 import QtWidgets, uic
+from PyQt5.QtCore import QTime, QDate
+import uuid
+import sqlite3
+from actions import Actions
+
+
+class ActionUI(QtWidgets.QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.aUi = uic.loadUi('design\\edit_event.ui')
+        self.user_events = {}
+
+        # Connecting sql database.
+        self.db = sqlite3.connect('database\\program.db')
+        self.crs = self.db.cursor()
+
+        # Settings for 'dE_date' control element.
+        self.aUi.dE_date.setCalendarPopup(True)
+        self.aUi.dE_date.setDate(QDate(QDate.currentDate()))
+        self.aUi.dE_date.setMaximumDate(QDate(QDate.currentDate()))
+
+        # Settings for 'tE_time' control element.
+        self.aUi.tE_time.setTime(QTime.currentTime())
+
+        # Settings for cB_category control element.
+        # self.aUi.cB_category.addItems()
+
+        self.aUi.btn_save.clicked.connect(self.add_event)
+
+        self.act = Actions
+
+        self.aUi.show()
+
+    def add_event(self):
+        userId = uuid.uuid4()
+
+        title = self.aUi.lE_name.text()
+        category = self.aUi.cB_category.currentText()
+        
+        time = self.aUi.tE_time.time()
+        hour = time.hour()
+        minute = time.minute()
+
+        date = self.aUi.dE_date.date()
+        year = date.year()
+        month = date.month()
+        day = date.day()
+
+        duration = self.aUi.lE_duration.text()        
+        comment = self.aUi.tE_comment.toPlainText()
+
+        a = self.act(title, category, hour, minute, year, month, day, \
+            duration, comment)
+        
+        # Testing some inserting data to db.
+        #self.crs.executemany("""INSERT INTO user(User_id, Instance) VALUES (?,?);""", ('2', '1'))
+        #self.db.commit()
+
+
+if __name__ == '__main__':
+    app = QtWidgets.QApplication(sys.argv)
+    win = ActionUI()
+    sys.exit(app.exec())
+    

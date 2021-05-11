c39fb2c91bd57f09b271c3b7ce47e392f54aca84
yurii-bel <dragonwow300@gmail.com>
2021-05-11 13:37:57 +0300

    Main window + add action done

--- logic/addding_editing_actions_ui.py
+++ logic/addding_editing_actions_ui.py
@@ -6,7 +6,7 @@ from PyQt5 import QtWidgets, uic
 from PyQt5.QtCore import QTime, QDate
 from PyQt5 import QtCore, QtGui, QtWidgets
 
-from logic.addding_editing_actions import Actions
+from addding_editing_actions import Actions
 from testing.actions_ui_test.edit import Ui_Form
 
 
--- main_testing.py
+++ main_testing.py
@@ -8,19 +8,26 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 
 from design.MainWindow import Ui_MainWindow  # importing main UI
 
-from logic.addding_editing_actions import Actions  
+from addding_editing_actions_ui import ActionUI  
 from testing.actions_ui_test.edit import Ui_Form  
 
 
 class MainUI(QtWidgets.QMainWindow):
     def __init__(self):
         super().__init__()
-        self.mUi = Ui_MainWindow()
+        self.setUi()
+
+    def setUi(self):
+        self.mUi = Ui_MainWindow()  # Main GUI class
         self.mUi.setupUi(self)
 
+        self.addAction = ActionUI()
+        print(self.mUi.btn_add_event)
+        self.mUi.btn_add_event.clicked.connect(self.open_add_action_window)
+        
     def open_add_action_window(self):
-        pass
-
+        self.addAction.show()
+    
 
 
 if __name__ == '__main__':

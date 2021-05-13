12340ff5367e0fad89ade0acabea25726219ef60
yurii-bel <dragonwow300@gmail.com>
2021-05-13 19:18:47 +0300

    another test

--- final_test/actions.py
+++ final_test/actions.py
@@ -1,6 +1,9 @@
 import sys
-from design.new_editUI import *
+sys.path.append(".")
+# from design.new_editUI import Ui_MainWindow
+from design.new_editUI_view import Ui_MainWindow
 from PyQt5.QtWidgets import QApplication, QMainWindow, QMessageBox, QTableView
+from PyQt5.QtWidgets import QTableWidget 
 from PyQt5 import QtSql
 from PyQt5 import QtCore
 from datetime import datetime
@@ -26,14 +29,14 @@ class form(QMainWindow):
         self.model.setHeaderData(2, QtCore.Qt.Horizontal,"Категория")
         self.model.setHeaderData(3, QtCore.Qt.Horizontal,"Потрачено времени")
         self.model.setHeaderData(4, QtCore.Qt.Horizontal,"Дата создания")
-        self.ui.tableWidget.setModel(self.model)
+        self.ui.tableView.setModel(self.model)
         self.ui.button_add.clicked.connect(self.addToDb)
         
         self.ui.button_refresh.clicked.connect(self.updaterow)
         self.ui.button_delete.clicked.connect(self.delrow)
         self.i = self.model.rowCount()
         self.ui.lcdNumber.display(self.i)
-        print(self.ui.tableWidget.currentIndex().row())
+        print(self.ui.tableView.currentIndex().row())
         self.show()
 
     def addToDb(self):
@@ -44,15 +47,15 @@ class form(QMainWindow):
         self.model.setData(self.model.index(self.i,2),
         self.ui.comboBox.currentText())  # str? # add text to db from comboBox
         self.model.setData(self.model.index(self.i,3),
-        self.ui.lineEdit_spend_time())  # add text to db from spend_time
+        self.ui.lineEdit_spend_time.text())  # add text to db from spend_time
         self.model.setData(self.model.index(self.i,4), 
         self.current_date)  # add text of today's date to db 
         self.model.submitAll()
         self.i += 1
         self.ui.lcdNumber.display(self.i)
-
+        print(self.ui.lineEdit_action_name.text())
     def delrow(self):
-        if self.ui.tableWidget.currentIndex().row() > -1:
+        if self.ui.tableView.currentIndex().row() > -1:
             self.model.removeRow(self.ui.tableWidget.currentIndex().row())
             self.i -= 1
             self.model.select()
@@ -63,13 +66,13 @@ class form(QMainWindow):
             self.show()
 
     def updaterow(self):
-        if self.ui.tableWidget.currentIndex().row() > -1:
-            record = self.model.record(self.ui.tableWidget.currentIndex().row())
+        if self.ui.tableView.currentIndex().row() > -1:
+            record = self.model.record(self.ui.tableView.currentIndex().row())
             record.setValue("Название события",self.ui.lineEdit_action_name)
             record.setValue("Категория",self.ui.comboBox.currentText())
             record.setValue("Потрачено времени", self.ui.lineEdit_spend_time)
             record.setValue("Дата создания", self.current_date)
-            self.model.setRecord(self.ui.tableWidget.currentIndex().row(), record)
+            self.model.setRecord(self.ui.tableView.currentIndex().row(), record)
         else:
             QMessageBox.question(self,'Message', 
             "Пожалуйста, выберите строку для обновления", QMessageBox.Ok)

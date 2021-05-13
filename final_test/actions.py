4c5fb65cee4dab18ee10c2a98d5fb9561708c95e
yurii-bel <dragonwow300@gmail.com>
2021-05-13 20:46:36 +0300

    sql lite db connected to code! add, change, delete work fine now!

--- final_test/actions.py
+++ final_test/actions.py
@@ -1,7 +1,8 @@
 import sys
+import sqlite3
 sys.path.append(".")
 # from design.new_editUI import Ui_MainWindow
-from design.new_editUI_view import Ui_MainWindow
+from design.new_editUI_view import *
 from PyQt5.QtWidgets import QApplication, QMainWindow, QMessageBox, QTableView
 from PyQt5.QtWidgets import QTableWidget 
 from PyQt5 import QtSql
@@ -9,17 +10,23 @@ from PyQt5 import QtCore
 from datetime import datetime
 
 
-
 class form(QMainWindow):
     def __init__(self):
         super().__init__()
         self.now = datetime.now()
-        self.current_date = self.now.strftime("%d/%m/%Y %H:%M:")
+        self.current_date = self.now.strftime("%d/%m/%Y %H:%M")
+
+        # Categories for cB_category control element.
+        self.categories = ['Здоровье', 'Работа', 'Семья', 'Отдых', 'Развлечения', \
+            'Другое']
 
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
+
         self.db = QtSql.QSqlDatabase.addDatabase('QSQLITE')
-        self.db.setDatabaseName('fieldlist.db')
+        self.db.setDatabaseName('fieldlista.db')
+
+        
         self.model = QtSql.QSqlTableModel()
         self.model.setTable('field')
         self.model.setEditStrategy(QtSql.QSqlTableModel.OnFieldChange)
@@ -37,40 +44,37 @@ class form(QMainWindow):
         self.i = self.model.rowCount()
         self.ui.lcdNumber.display(self.i)
         print(self.ui.tableView.currentIndex().row())
+
+        self.ui.comboBox.addItems(self.categories)
         self.show()
 
     def addToDb(self):
         print(self.i)
         self.model.insertRows(self.i,1)
-        self.model.setData(self.model.index(self.i,1), 
-        self.ui.lineEdit_action_name.text())  # add text to db from action_name
-        self.model.setData(self.model.index(self.i,2),
-        self.ui.comboBox.currentText())  # str? # add text to db from comboBox
-        self.model.setData(self.model.index(self.i,3),
-        self.ui.lineEdit_spend_time.text())  # add text to db from spend_time
-        self.model.setData(self.model.index(self.i,4), 
-        self.current_date)  # add text of today's date to db 
+        self.model.setData(self.model.index(self.i,1), self.ui.lineEdit_action_name.text())  # add text to db from action_name
+        self.model.setData(self.model.index(self.i,2), self.ui.comboBox.currentText())  # str? # add text to db from comboBox
+        self.model.setData(self.model.index(self.i,3), self.ui.lineEdit_spend_time.text())  # add text to db from spend_time
+        self.model.setData(self.model.index(self.i,4), self.current_date)  # add text of today's date to db 
         self.model.submitAll()
         self.i += 1
         self.ui.lcdNumber.display(self.i)
-        print(self.ui.lineEdit_action_name.text())
+
     def delrow(self):
         if self.ui.tableView.currentIndex().row() > -1:
-            self.model.removeRow(self.ui.tableWidget.currentIndex().row())
+            self.model.removeRow(self.ui.tableView.currentIndex().row())
             self.i -= 1
             self.model.select()
             self.ui.lcdNumber.display(self.i)
         else:
-            QMessageBox.question(self,'Message', 
-            "Пожалуйста, выберите строку для удаления", QMessageBox.Ok)
+            QMessageBox.question(self,'Message', "Пожалуйста, выберите строку для удаления", QMessageBox.Ok)
             self.show()
 
     def updaterow(self):
         if self.ui.tableView.currentIndex().row() > -1:
             record = self.model.record(self.ui.tableView.currentIndex().row())
-            record.setValue("Название события",self.ui.lineEdit_action_name)
+            record.setValue("Название события",self.ui.lineEdit_action_name.text())
             record.setValue("Категория",self.ui.comboBox.currentText())
-            record.setValue("Потрачено времени", self.ui.lineEdit_spend_time)
+            record.setValue("Потрачено времени", self.ui.lineEdit_spend_time.text())
             record.setValue("Дата создания", self.current_date)
             self.model.setRecord(self.ui.tableView.currentIndex().row(), record)
         else:

9c13d4744501984881fb9d82a2510014b3ee4092
timo364 <timoxa.or12@gmail.com>
2021-05-17 23:20:19 +0300

    last commit in this branch... bye!

--- logic/addding_editing_actions_ui.py
+++ logic/addding_editing_actions_ui.py
@@ -9,16 +9,16 @@ from PyQt5 import QtWidgets, uic
 from logic.addding_editing_actions import Actions
 
 
-class ActionUI(QtWidgets.QMainWindow):
+class ActionsUI(QtWidgets.QMainWindow):
+    '''
+    This class implements adding and editing actions.
+    '''
     def __init__(self):
         super().__init__()
 
+        # Loading appropriate UI's.
         self.aUi = uic.loadUi('design\\add_event.ui')
         self.eUi = uic.loadUi('design\\edit_event.ui')
-
-        # This method of showing ui is broken.
-        # self.eUi = EditUI()
-        # self.eUi.setupUi(self)
 
         # For time of event cration in a_save_event.
         time_ = time.localtime()
@@ -28,26 +28,29 @@ class ActionUI(QtWidgets.QMainWindow):
         self.db = sqlite3.connect('database\\timo364\\TimeSoft.db')
         self.curs = self.db.cursor()
 
-        # Categories for cB_category control element.
+        # Categories for 'a_cB_category' control element.
         self.categs = ['Здоровье', 'Работа', 'Семья', 'Отдых', 'Развлечения', \
             'Другое']
 
-        # Settings for 'dE_date' control element.
-        self.aUi.dE_date.setCalendarPopup(True)
-        self.aUi.dE_date.setDate(QDate(QDate.currentDate()))
-        self.aUi.dE_date.setMaximumDate(QDate(QDate.currentDate()))
+        # Settings for 'a_dE_date' control element.
+        self.aUi.a_dE_date.setCalendarPopup(True)
+        self.aUi.a_dE_date.setDate(QDate(QDate.currentDate()))
+        self.aUi.a_dE_date.setMaximumDate(QDate(QDate.currentDate()))
 
-        # Settings for cB_category control element.
-        self.aUi.cB_category.addItems(self.categs)
+        # Settings for 'a_cB_category' control element.
+        self.aUi.a_cB_category.addItems(self.categs)
 
-        self.aUi.btn_save.clicked.connect(self.add_event)
-        self.aUi.btn_cancel.clicked.connect(self.aUi.close)
+        self.aUi.a_btn_save.clicked.connect(self.add_event)
+        self.aUi.a_btn_cancel.clicked.connect(self.aUi.close)
+        self.aUi.a_btn_exit.clicked.connect(self.aUi.close)
 
+        # Instance of main logic.
         self.act = Actions
 
     def add_event(self):
-        title = self.aUi.lE_name.text()
-        category = self.aUi.cB_category.currentText()
+        # Getting all info, entered by user. 
+        title = self.aUi.a_lE_name.text()
+        category = self.aUi.a_cB_category.currentText()
 
         hour_ = int(self._time[:2])
         minute_ = int(self._time[3:5])
@@ -56,17 +59,19 @@ class ActionUI(QtWidgets.QMainWindow):
         minute = minute_
         second = second_
         
-        date = self.aUi.dE_date.date()
+        date = self.aUi.a_dE_date.date()
         year = date.year()
         month = date.month()
         day = date.day()
         
-        duration = self.aUi.lE_duration.text()        
-        comment = self.aUi.tE_comment.toPlainText()
+        duration = self.aUi.a_lE_duration.text()        
+        comment = self.aUi.a_tE_comment.toPlainText()
 
+        # Using main logic and writing all recieved info.
         self.added_event = self.act(title, category, hour, minute, second, \
             year, month, day, duration, comment)
 
+        # Using test database!
         # Writing created event to db.
         data = (self.added_event.action, \
             self.added_event.category, self.added_event.duration, \
@@ -75,17 +80,16 @@ class ActionUI(QtWidgets.QMainWindow):
         self.curs.execute('insert into Actions values(?,?,?,?,?,?)', \
             data)       
 
+        # Writing all changes to db and closing 'Add Event' win.
         self.db.commit()
         self.aUi.close()
 
-    def close_add_event(self):
-        self.aUi.close()
-
     def edit_event(self):
+        # This method is reserved for future editing actions.
         pass
 
 if __name__ == '__main__':
     app = QtWidgets.QApplication(sys.argv)
-    win = ActionUI()
+    win = ActionsUI()
     sys.exit(app.exec())
     
--- timeSoft.py
+++ timeSoft.py
@@ -1,23 +1,44 @@
 import sys
+
 sys.path.append(".")
 
-from PyQt5 import QtWidgets, uic
+from PyQt5 import QtGui, QtWidgets, uic
 
-from logic.addding_editing_actions_ui import ActionUI
+from logic.addding_editing_actions_ui import ActionsUI
 
 class MainUI(QtWidgets.QMainWindow):
+    '''
+    This class implements Main Window of TimeSoft program.
+    '''
     def __init__(self):
         super().__init__()
 
-        self.mUi = uic.loadUi('design\\main.ui')
-        self.aUi = ActionUI()
+        # Loading UI's.
+        self.mUi = uic.loadUi('design\\MainWindow.ui')
+        self.aUi = ActionsUI()
+        self.rUi = uic.loadUi('design\\register.ui')
+        self.lUi = uic.loadUi('design\\login.ui')
+        self.sUi = uic.loadUi('design\\settings.ui')
 
+        # Connecting buttons to slots.
+        # All commented stuff is not yet realized.
         self.mUi.btn_plus.clicked.connect(self.add_action)
+        #self.mUi.btn_forecast.clicked.connect(self.forecasting)
+        #self.mUi.btn_background.clicked.connect(self.change_theme)
+        self.mUi.btn_settings.clicked.connect(self.settings)
+        self.mUi.btn_exit.clicked.connect(self.mUi.close)
         
+        # Setting pixmap for 'lbl_logoimg' element.
+        _ = QtGui.QPixmap('design\\img\\icons\\Logo.png')
+        self.mUi.lbl_logoimg.setPixmap(_)
+
         self.mUi.show()
 
     def add_action(self):
         self.aUi.aUi.show()
+
+    def settings(self):
+        self.sUi.show()
 
 
 if __name__ == '__main__':

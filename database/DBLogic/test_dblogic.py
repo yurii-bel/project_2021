cad4784634a1d5910060ac1848af64106e344eb3
Vlad <tyronvv@krok.edu.ua>
2021-05-18 22:43:48 +0300

    Carcass for db logic

--- database/DBLogic/test_dblogic.py
+++ database/DBLogic/test_dblogic.py
@@ -1,1 +1,22 @@
-print("test")
+import psycopg2
+
+Class DbLogic:
+
+def __init__(db_name, db_user, db_password, db_host, db_port):
+    connection = None
+    try:
+        connection = psycorg2.connect(database = db_name, user = db_user,
+        password = db_password, host = db_host, port = db_port)
+        print('Successfull connection to database')
+    except psycopg2.OperationalError as e:
+        print(f'An error {e} occurred')
+
+def set_data():
+    None
+
+def get_data():
+    None
+
+def del_data():
+    None
+

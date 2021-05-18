4e740a8109e1c8f5f63b5c30dc20aee13bb35a5c
timo364 <timoxa.or12@gmail.com>
2021-05-18 18:30:25 +0300

    wtf?

--- logic/time_db.py
+++ logic/time_db.py
@@ -8,29 +8,30 @@ class TimeDb:
 
         self.cursor = self.db.cursor()
     
-    def set_user_data(self, *args, **kwargs):
+    def set_data(self, *args, **kwargs):
 
         args_and_table = (args)  # arguments + table
-        table_name = list(args_and_table)[-1]  # table 
-        args = list(args_and_table).pop()  # aeguments - table
+        table_name = list(args_and_table).pop()  # table 
+        args = list(args_and_table)[:-1]  # arguments - table
         prosseco = '?,'
 
         # pushing any number of arguments into db
-        for i in range(len(args_and_table)-2):
-            if i < len(args_and_table)-3:
-                prosseco += '?,'
-            else:
-                prosseco += '?'
-        
-        print(type(args_and_table))
+        if len(args) == 1:
+            prosseco = '?'
+        elif len(args) == 2:
+            prosseco += '?'
+        elif len(args_and_table) >= 3:
+            for i in range(len(args_and_table)-2):
+                if i < len(args_and_table)-3:
+                    prosseco += '?,'
+                else:
+                    prosseco += '?'
+
         self.cursor.execute(f'insert into {table_name} values({prosseco})', \
             args)
         self.db.commit()       
 
-    
-
-
 
 if __name__ == '__main__':
     time_db = TimeDb()
-    time_db.set_user_data('dsa', 'em@gmail.com', '123', '32jd2dj', 'User')
+    time_db.set_data('dsa', 'q', '3', '4', '5', 'Activity')

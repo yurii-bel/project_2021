ea6f9e4b0b995ede66fa9b04f516a9adbe18e3ae
yurii-bel <dragonwow300@gmail.com>
2021-05-18 19:57:14 +0300

    TimeDb completed!

--- logic/time_db.py
+++ logic/time_db.py
@@ -3,34 +3,58 @@ sys.path.append(".")
 import sqlite3
 
 class TimeDb:
-    def __init__(self):
-        self.db = sqlite3.connect('database\\yurii_bel\\time_db.db')
-
+    def __init__(self, db):
+        self.db = sqlite3.connect(f'{db}')
         self.cursor = self.db.cursor()
     
-    def set_user_data(self, *args, **kwargs):
+    def set_data(self, *args):
 
         args_and_table = (args)  # arguments + table
-        table_name = list(args_and_table)[-1]  # table 
-        args = list(args_and_table).pop()  # aeguments - table
-        prosseco = '?,'
+        table_name = list(args_and_table).pop()  # table 
+        args = list(args_and_table)[:-1]  # arguments - table
+        symbol = '?,'
 
         # pushing any number of arguments into db
-        for i in range(len(args_and_table)-2):
-            if i < len(args_and_table)-3:
-                prosseco += '?,'
-            else:
-                prosseco += '?'
-        
-        print(type(args_and_table))
-        self.cursor.execute(f'insert into {table_name} values({prosseco})', \
+        if len(args) == 1:
+            symbol = '?'
+        elif len(args) == 2:
+            symbol += '?'
+        elif len(args_and_table) >= 3:
+            for i in range(len(args_and_table)-2):
+                if i < len(args_and_table)-3:
+                    symbol += '?,'
+                else:
+                    symbol += '?'
+
+        self.cursor.execute(f'insert into {table_name} values({symbol})', \
             args)
         self.db.commit()       
 
-    
+    def get_data(self, *args):
 
+        args_and_table = (args)  # arguments + table
+        table_name = list(args_and_table).pop()  # table 
+        args = list(args_and_table)[:-1]  # arguments - table
+   
+        sql = f'SELECT {str(args)[2:-2]} FROM {table_name}'
+        self.cursor.execute(sql)
+        full_data = self.cursor.fetchall()
+        return str(full_data)[3:-4]
+
+    def del_data(self, *args):
+        args_and_table = (args)  # arguments + table
+        table_name = list(args_and_table).pop()  # table 
+        args = list(args_and_table)[:-1]  # arguments - table
+        args_first = args[0]
+        args_second = args[1]
+
+        sql = f'DELETE FROM {table_name} WHERE {args_first} = "{args_second}"'
+        self.cursor.execute(sql)
+        self.db.commit()      
 
 
 if __name__ == '__main__':
-    time_db = TimeDb()
-    time_db.set_user_data('dsa', 'em@gmail.com', '123', '32jd2dj', 'User')
+    time_db = TimeDb('database\\yurii_bel\\time_db.db')
+    # time_db.set_data('John', 'john@gmail.com', '123', 'telegramm', 'User')
+    # time_db.get_data('user_email', 'User')
+    # time_db.del_data('user_name', 'John', 'User')

7c5e001114b64031c656d458fb4fb6f289d9fe11
Vlad <tyronvv@krok.edu.ua>
2021-05-18 23:44:29 +0300

    mm

--- database/DBLogic/dblogic.py
+++ database/DBLogic/dblogic.py
@@ -1,22 +1,27 @@
 import psycopg2
 
-Class DbLogic:
 
-def __init__(db_name, db_user, db_password, db_host, db_port):
-    connection = None
-    try:
-        connection = psycorg2.connect(database = db_name, user = db_user,
-        password = db_password, host = db_host, port = db_port)
-        print('Successfull connection to database')
-    except psycopg2.OperationalError as e:
-        print(f'An error {e} occurred')
+class DbLogic:
 
-def set_data():
-    None
+    def __init__(db_name, db_user, db_password, db_host, db_port):
+        connection = None
+        try:
+            connection = psycorg2.connect(database = db_name,
+            user = db_user,
+            password = db_password,
+            host = db_host,
+            port = db_port)
+            print('Successfull connection to database')
+        except psycopg2.OperationalError as e:
+            print(f'An error {e} occurred')
 
-def get_data():
-    None
+    def set_data():
+        None
 
-def del_data():
-    None
+    def get_data():
+        None
 
+    def del_data():
+        None
+
+db_connect = DbLogic('Timesoft', 'postgres', '111', '127.0.0.1', '5432')

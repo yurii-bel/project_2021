46c1f97e13aad75509bf84877a5cb85bac666979
Vlad <tyronvv@krok.edu.ua>
2021-05-22 01:13:01 +0300

    working database connection with outline of data processing methods

--- database/DBLogic/dblogic.py
+++ database/DBLogic/dblogic.py
@@ -1,27 +1,39 @@
-import psycopg2
+import psycopg2 as db
 
 
 class DbLogic:
 
-    def __init__(db_name, db_user, db_password, db_host, db_port):
-        connection = None
-        try:
-            connection = psycorg2.connect(database = db_name,
-            user = db_user,
-            password = db_password,
-            host = db_host,
-            port = db_port)
-            print('Successfull connection to database')
-        except psycopg2.OperationalError as e:
-            print(f'An error {e} occurred')
+    database = 'deikj6tsb8tesq'
+    user = 'swvxsrergazlio'
+    password = 'd59791f7927ca5f5e8491bbbe93fbd93ea62e00a08821326f2aacc81c4307057'
+    host = 'ec2-54-216-185-51.eu-west-1.compute.amazonaws.com'
+#рабочий коннект
+    def __init__(self):
+        self.connection = db.connect(database = self.database,
+                                            user = self.user,
+                                            password = self.password,
+                                            host = self.host)
 
-    def set_data():
-        None
+        self.cursor = self.connection.cursor()
 
-    def get_data():
-        None
+#Не трогать, не работает пока что
+    # def set_data(self, table_name, stolbec, *args):
+    #     table_name = table_name
+    #     table_record = ', '.join(['%s']*len(args))
+    #     insert_query = (f'(INSERT INTO "{table_name}" {stolbec} VALUES {table_record})')
+    #     self.connection.autocommit = True
+    #     cursor = self.connection.cursor()
+    #     cursor.execute(insert_query, args)
 
-    def del_data():
-        None
+#наброски
+    def get_data(self, table_name):
+        self.cursor.execute(f'(SELECT * FROM "{table_name}")')
+        return self.cursor.fetchall()
 
-db_connect = DbLogic('Timesoft', 'postgres', '111', '127.0.0.1', '5432')
+    def del_data(self, table_name, condition):
+        self.cursor.execute(f'(DELETE FROM "{table_name}" WHERE {condition} ')
+        return self.cursor.fetchall()
+
+    def __stop__(self):
+        self.connection.close()
+db_conn = DbLogic()

eab2e8825b1bd3156e93d6277266fdf2b9869f97
Sif-on <e.simonovskiy@gmail.com>
2021-05-23 23:00:07 +0300

    Добавил запросы для заполнения БД

---
+++ database/Zapros/zapros_create_USER_NAME.sql
@@ -0,0 +1,10 @@
+INSERT INTO "USER_NAME" (USER_N_ID, USER_N_name, USER_N_Telegram) VALUES ('996MdkMfxhWRZTD8G0F3', 'Sif', (NULL)),
+('sfY4P2il6J1lApuyirOe', 'Timo', (NULL)),
+('YK3xSwFVdmQthAgrQdGu', 'Владимир', (NULL)),
+('yMWK3dVFj0YAyZVJGOOF', 'Ева', (NULL)),
+('laK3i8m70MWV2ExXHvsa', 'Андрей', (NULL)),
+('fq7bonIQekCAUhIx4kzl', 'Лидия', '30389856562196918872'),
+('E5hoTWtbwN3XtdDGmqc2', 'София', (NULL)),
+('V1AID6bYf8oO2M3doofS', 'Тимофей', '87847091087610956697'),
+('z7IyGgcbog2udS3MGwLe', 'Василиса', '52500656816483114646'),
+('TNreeXTq78iczsWL0sqD', 'Кирилл', (NULL))

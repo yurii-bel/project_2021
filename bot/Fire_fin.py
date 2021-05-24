97def5bf05e3346a8198ca7ae8815398da62a7d5
Sif-on <e.simonovskiy@gmail.com>
2021-05-24 17:34:06 +0300

    обновил доступ к БД для бота

--- bot/Fire_fin.py
+++ bot/Fire_fin.py
@@ -1,24 +1,25 @@
-tk = '1891194594:AAGDaHHTuZv5mDBIV8Q4Sjd7xU1RPPry0ns'
-import psycopg2 as db
-from telebot.types import User
 import telebot
+from telebot.types import User
+import psycopg2 as db
+tk = '1891194594:AAGDaHHTuZv5mDBIV8Q4Sjd7xU1RPPry0ns'
 
-db_name='deikj6tsb8tesq'
-db_pass='d59791f7927ca5f5e8491bbbe93fbd93ea62e00a08821326f2aacc81c4307057'
-db_user='swvxsrergazlio'
-db_host='ec2-54-216-185-51.eu-west-1.compute.amazonaws.com'
+database = 'dt1vdgsvah47r'
+user = 'ryxcgrjdgvrsxx'
+password = '2e4d8cbc5b0f94259507584c6868f20ae0d4da79fdc618f6c2602d18045b2b61'
+host = 'ec2-54-74-60-70.eu-west-1.compute.amazonaws.com'
 
-#подключение к бд на хироку
-connection = db.connect(database=db_name, user=db_user, password=db_pass, host=db_host)
+# подключение к бд на хироку
+connection = db.connect(database=db_name, user=db_user,
+                        password=db_pass, host=db_host)
 
-#подключение курсора для работы с бд
+# подключение курсора для работы с бд
 cur = connection.cursor()
 
 #cur.execute('CREATE TABLE ACTIVITY(id SERIAL PRIMARY KEY, name VARCHAR);')
 #cur.execute('INSERT INTO ACTIVITY (name) VALUES(%s)', ('eq',))
 cur.execute('SELECT * FROM "USER";')
 
-#вывод всех строк из таблицы
+# вывод всех строк из таблицы
 (cur.fetchall())
 
 connection.commit()
@@ -31,25 +32,24 @@ connection.close()
 bot = telebot.TeleBot(tk)
 
 
-
 @bot.message_handler(commands=['start'])
 def welcome_help(message):
     bot.send_message(message.chat.id, "Your'e started your bot")
 
-#TODO:i need to fix 'connection'
+# TODO:i need to fix 'connection'
 #connection is lagged
+
+
 @bot.message_handler(commands=['get_me'])
-def get_message(message,connection):
+def get_message(message, connection):
     if message.chat.id == 'name':
         cursor = connection.cursor()
-        cur.execute("SELECT * FROM USER_NAME  WHERE id = %s;",(2,))
+        cur.execute("SELECT * FROM USER_NAME  WHERE id = %s;", (2,))
         bot.send_message(message.chat.id, print(cur.fetchone().cursor))
         connection.commit()
         connection.close
     else:
         print('Please try again!')
-    
-
 
 
 if __name__ == '__main__':

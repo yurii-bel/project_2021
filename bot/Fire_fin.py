12424f8c69cf1f8f921f2895bff7a69e83d8f2f6
NUBMAN <pechkursasha@gmail.com>
2021-05-25 23:25:14 +0300

    .

--- bot/Fire_fin.py
+++ bot/Fire_fin.py
@@ -1,48 +1,41 @@
 import telebot
 from telebot.types import User
 import psycopg2 as db
+import time
 tk = '1891194594:AAGDaHHTuZv5mDBIV8Q4Sjd7xU1RPPry0ns'
 
 
-class TelegramBot:
 
-    database = 'dt1vdgsvah47r'
-    user = 'ryxcgrjdgvrsxx'
-    password = '2e4d8cbc5b0f94259507584c6868f20ae0d4da79fdc618f6c2602d18045b2b61'
-    host = 'ec2-54-74-60-70.eu-west-1.compute.amazonaws.com'
 
-    def __init__(self):
-        self.connection = db.connect(database=self.database,
-                                     user=self.user,
-                                     password=self.password,
-                                     host=self.host)
+db_name = 'dt1vdgsvah47r'
+db_user = 'ryxcgrjdgvrsxx'
+db_password = '2e4d8cbc5b0f94259507584c6868f20ae0d4da79fdc618f6c2602d18045b2b61'
+db_host = 'ec2-54-74-60-70.eu-west-1.compute.amazonaws.com'
 
-        self.cursor = self.connection.cursor()
 
-        self.correct_login_info = False
+connection = db.connect(database = db_name, user = db_user, password = db_password, host = db_host)
 
-    bot = telebot.TeleBot(tk)
+cursor = connection.cursor()
+correct_login_info = False
+bot = telebot.TeleBot(tk)
 
-    @bot.message_handler(commands=['start'])
-    def welcome_help(message):
-        bot.send_message(message.chat.id, "Your'e started your bot")
+@bot.message_handler(command = 'categories')
+def add_action(message):
+    name = message.from_user.id
+    cursor.execute("""SELECT (user_id,cat) TABLE IF NOT EXIST CATEGORY(
+        user_id INTEGER
+        cat_name STRING
+        )""")
 
-    # TODO:i need to fix 'connection'
-    #connection is lagged
-
-    @bot.message_handler(commands=['get_me'])
-    def get_message(message, connection):
-        if message.chat.id == 'name':
-            cursor = connection.cursor()
-            cur.execute("SELECT * FROM USER_NAME  WHERE id = %s;", (2,))
-            bot.send_message(message.chat.id, print(cur.fetchone().cursor))
-            connection.commit()
-            connection.close
-        else:
-            print('Please try again!')
+    user_abilities = [message.chat.user_id, message.chat.cat_name]
+    cursor.execute("INSERT INTO CATEGORY VALUES(%s,%s);",user_abilities)
+    db.connect.commit()
 
 
 if __name__ == '__main__':
-    telegrambot = TelegramBot
+ while True:
+    try:
+        bot.infinity_polling(True)
+    except Exception:
+        time.sleep(15)
 
-    bot.telegrambot.infinity_polling(True)

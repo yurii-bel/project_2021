7f00dda764be49d221aec7bee45ca0317fb590c1
Sif-on <e.simonovskiy@gmail.com>
2021-06-01 01:25:07 +0300

    Исправил баг с запуском сайта на сервере и лого

--- gettingstarted/settings.py
+++ gettingstarted/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
-    # "django.contrib.staticfiles",
+    "django.contrib.staticfiles",
     "web",
 ]
 

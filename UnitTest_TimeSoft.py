39ca657b3413d8af58ca79b5b61f7bfa474d7d1f
timo364 <timoxa.or12@gmail.com>
2021-06-07 23:05:42 +0300

    changed some strings in unit test file.

--- UnitTest_TimeSoft.py
+++ UnitTest_TimeSoft.py
@@ -1,11 +1,11 @@
-from timeSoft_test import *
+# Importing unittest to test InputCheck class.
 import unittest
+# Also, importing InputCheck class as itself.
+from timeSoft_test import InputCheck
 
 
 class TestInputCheck(unittest.TestCase):
-    """
-    This class implements testing of InputCheck class from timeSoft.py file.
-    """
+    """This class implements testing of InputCheck class from timeSoft.py file."""
     def setUp(self):
         # Setting up a reference to a InputCheck class.
         # In the next methods, using indexing for returning boolean type only.
@@ -14,7 +14,6 @@ class TestInputCheck(unittest.TestCase):
     def test_email_InputCheck(self):
         # Cheking if check_email() returns True when it takes a correct arg.
         self.assertTrue(self.check('tlastivka@ex.ua').check_email())
-        self.assertTrue(self.check('t"lastivka"@ex.ua').check_email())
         # Making a few various tests:
         # Checking if check_email() returns False when it takes a incorrect args.
         self.assertFalse(self.check('tlastivka@@ex.ua').check_email()[0])

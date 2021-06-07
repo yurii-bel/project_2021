2fed53c2abb52aeb04eb59d0302b6d6807e5b85f
NUBMAN <pechkursasha@gmail.com>
2021-06-07 14:42:20 +0300

    Делает импорт timeSoft_test относительным

--- bot/bot.py
+++ bot/bot.py
@@ -10,7 +10,7 @@ import configparser
 from datetime import datetime, timedelta
 from telebot import types
 
-from timeSoft_test import InputCheck
+from ..timeSoft_test import InputCheck
 
 
 config = configparser.ConfigParser()
